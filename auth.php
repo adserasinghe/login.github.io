2c58046d5c56f2f2763e189960041e3831ca068a
Aditha Dinuja Serasinghe <svadithadinujaserasinghe@gmail.com>
2024-07-29 22:19:45 +0530

    database changed...

--- auth.php
+++ auth.php
@@ -3,7 +3,7 @@ session_start();
 $DATABASE_HOST = 'sql110.infinityfree.com';
 $DATABASE_USER = 'if0_36822891';
 $DATABASE_PASS = '#Adsads2002#';
-$DATABASE_NAME = 'if0_36822891_adserasingheDB';
+$DATABASE_NAME = 'accounts';
 $conn='index.php';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
