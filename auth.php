da5c9a5a842043bf648858ae448e0b9520cf5ab7
Aditha Dinuja Serasinghe <svadithadinujaserasinghe@gmail.com>
2024-07-29 18:49:05 +0530

    some links added

--- auth.php
+++ auth.php
@@ -4,6 +4,7 @@ $DATABASE_HOST = 'mysql-11-10-adserasinghe.f.aivencloud.com';
 $DATABASE_USER = 'avnadmin';
 $DATABASE_PASS = 'AVNS_7YdXCnU7qGyOILgsZRd';
 $DATABASE_NAME = 'defaultdb';
+$conn='index.php';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
