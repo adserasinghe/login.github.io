37b1294fe4685da1334fe2154dc51c55f45651a8
Aditha Dinuja Serasinghe <svadithadinujaserasinghe@gmail.com>
2024-07-29 16:01:49 +0530

    Update auth.php
    auth some lines added...

--- auth.php
+++ auth.php
@@ -7,6 +7,10 @@ $DATABASE_NAME = 'defaultdb';
 $uri = "mysql://avnadmin:************************@mysql-11-10-adserasinghe.f.aivencloud.com:11131/defaultdb?ssl-mode=REQUIRED";
 $fields = parse_url($uri);
 $conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
+$conn = "mysql:";
+$conn .= "host=" . $fields["host"];
+$conn .= ";port=" . $fields["port"];;
+$conn .= ";dbname=defaultdb";
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
