49252098327345446d0f89ddc7358b30ea69ac63
Aditha Dinuja Serasinghe <svadithadinujaserasinghe@gmail.com>
2024-07-29 18:47:48 +0530

    index2 added

--- auth.php
+++ auth.php
@@ -4,13 +4,6 @@ $DATABASE_HOST = 'mysql-11-10-adserasinghe.f.aivencloud.com';
 $DATABASE_USER = 'avnadmin';
 $DATABASE_PASS = 'AVNS_7YdXCnU7qGyOILgsZRd';
 $DATABASE_NAME = 'defaultdb';
-$uri = "mysql://avnadmin:************************@mysql-11-10-adserasinghe.f.aivencloud.com:11131/defaultdb?ssl-mode=REQUIRED";
-$fields = parse_url($uri);
-$conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
-$conn = "mysql:";
-$conn .= "host=" . $fields["host"];
-$conn .= ";port=" . $fields["port"];;
-$conn .= ";dbname=defaultdb";
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
---
+++ index2.php
@@ -0,0 +1,21 @@
+<?php
+
+$uri = "mysql://avnadmin:AVNS_7YdXCnU7qGyOILgsZRd@mysql-11-10-adserasinghe.f.aivencloud.com:11131/defaultdb?ssl-mode=REQUIRED";
+
+$fields = parse_url($uri);
+
+// build the DSN including SSL settings
+$conn = "mysql:";
+$conn .= "host=" . $fields["host"];
+$conn .= ";port=" . $fields["port"];;
+$conn .= ";dbname= defaultdb";
+$conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
+
+try {
+  $db = new PDO($conn, $fields["user"], $fields["pass"]);
+
+  $stmt = $db->query("SELECT VERSION()");
+  print($stmt->fetch()[0]);
+} catch (Exception $e) {
+  echo "Error: " . $e->getMessage();
+}
